ba367097723df69e8e3fa5b881e4d08e5e8ad904
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-10-03 23:00:35 +0400

    :rocket: added bloom post processing fixed mode issues removed light in hero section canvas

--- components/pages/landing/hero/Hero.landin.tsx
+++ components/pages/landing/hero/Hero.landin.tsx
@@ -10,7 +10,9 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+
+
 
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger)
@@ -30,8 +32,6 @@ const Hero = () => {
     const [cursor,] = useState({ x: 0, y: 0 })
 
 
-
-
     useEffect(() => {
         init();
     }, []);
@@ -41,9 +41,8 @@ const Hero = () => {
 
 
     const init = () => {
-        // TODO: use leva instead of this
+        //! TODO: get rid of leva 
         const canvas = document.querySelector(".landing_canvas") as HTMLElement;
-        const clock = new THREE.Clock();
 
 
         const sizes = {
@@ -51,45 +50,30 @@ const Hero = () => {
             height: window.innerHeight
         }
 
+        const postProcessingParams = {
+            exposure: 5,
+            bloomStrength: 0.3,
+            bloomThreshold: 0,
+            bloomRadius: 1
+        };
+
+
+
+        //============================ RENDERER
+        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true })
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        renderer.toneMappingExposure = 1.25;
+        renderer.setSize(sizes.width, sizes.height);
+
+
 
         //============================ SCENE
         const scene = new THREE.Scene();
         scene.background = new THREE.Color('#020305');
 
         scene.fog = new THREE.Fog(0x000000, 10, 20);
-
-
-
-
-
-        //============================ LIGHT
-        const light = new THREE.DirectionalLight(0x1253FE, 5);
-        light.position.set(0, 1, 0); //default; light shining from top
-        light.castShadow = true; // default false
-        scene.add(light);
-
-
-        let pointLigtht1 = new THREE.PointLight(0xE05289, 10);
-        pointLigtht1.position.set(10, 10, 10);
-        pointLigtht1.castShadow = true
-        scene.add(pointLigtht1)
-
-        let pointLigtht2 = new THREE.PointLight(0x3357FA, 10);
-        pointLigtht2.position.set(-10, 5, 0);
-        pointLigtht2.castShadow = true
-        scene.add(pointLigtht2)
-
-        let pointLigtht3 = new THREE.PointLight(0x3357FA, 10);
-        pointLigtht3.position.set(0, 5, 0);
-        pointLigtht3.castShadow = true
-        pointLigtht3.scale.set(10, 0, 10);
-
-        scene.add(pointLigtht3)
-
-        var lighth = new THREE.HemisphereLight(0xf6e86d, 0x404040, 15);
-        scene.add(lighth);
-
-
 
 
 
@@ -104,13 +88,75 @@ const Hero = () => {
 
 
 
-        //============================ RENDERER
-        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true })
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        renderer.outputEncoding = THREE.sRGBEncoding;
-        renderer.toneMapping = THREE.ACESFilmicToneMapping;
-        renderer.toneMappingExposure = 1.25;
-        renderer.setSize(sizes.width, sizes.height);
+        //============================ POSTPROCESSING 
+
+
+        let composer = new EffectComposer(renderer);
+        const renderPass = new RenderPass(scene, camera);
+        composer.addPass(renderPass);
+        const renderScene = new RenderPass(scene, camera);
+
+        const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+        bloomPass.threshold = postProcessingParams.bloomThreshold;
+        bloomPass.strength = postProcessingParams.bloomStrength;
+        bloomPass.radius = postProcessingParams.bloomRadius;
+
+        composer = new EffectComposer(renderer);
+        composer.addPass(renderScene);
+        composer.addPass(bloomPass);
+
+
+        // const bokehPass = new BokehPass(scene, camera, {
+        //     focus: 0.15,
+        //     aperture: 0.025,
+        //     maxblur: 0.01,
+
+        //     width: window.innerWidth,
+        //     height: window.innerHeight
+        // });
+        // composer.addPass(renderPass);
+        // composer.addPass(bokehPass);
+
+
+
+
+
+
+
+
+
+        //============================ LIGHT
+        // const light = new THREE.DirectionalLight(0x1253FE, 5);
+        // light.position.set(0, 1, 0); //default; light shining from top
+        // light.castShadow = true; // default false
+        // scene.add(light);
+
+
+        // let pointLigtht1 = new THREE.PointLight(0xE05289, 10);
+        // pointLigtht1.position.set(10, 10, 10);
+        // pointLigtht1.castShadow = true
+        // scene.add(pointLigtht1)
+
+        // let pointLigtht2 = new THREE.PointLight(0x3357FA, 10);
+        // pointLigtht2.position.set(-10, 5, 0);
+        // pointLigtht2.castShadow = true
+        // scene.add(pointLigtht2)
+
+        // let pointLigtht3 = new THREE.PointLight(0x3357FA, 10);
+        // pointLigtht3.position.set(0, 5, 0);
+        // pointLigtht3.castShadow = true
+        // pointLigtht3.scale.set(10, 0, 10);
+
+        // scene.add(pointLigtht3)
+
+        var lighth = new THREE.HemisphereLight(0xf6e86d, 0x404040, 15);
+        scene.add(lighth);
+
+
+
+
+
+
 
 
 
@@ -150,8 +196,8 @@ const Hero = () => {
                 color: 0x000000,
                 metalnessMap: roughnessMapTexture,
                 roughness: 0.21,
-                normalMap: normalMapTexture,
-                normalScale: new THREE.Vector2(0.2, 0.2),
+                // normalMap: normalMapTexture,
+                // normalScale: new THREE.Vector2(0.2, 0.2),
                 emissive: 0x3357FA,
                 emissiveIntensity: 0.005,
                 envMap: envmap.texture,
@@ -192,6 +238,7 @@ const Hero = () => {
                     gltf.scene.position.set(0, 3, 0);
                     gltf.scene.scale.set(2, 2, 2);
 
+
                     scene.traverse((child: THREE.Object3D) => {
                         if ((child as THREE.Mesh).isMesh) {
                             (child as THREE.Mesh).material = logoMaterial;
@@ -219,13 +266,10 @@ const Hero = () => {
 
 
 
-        //============================ POSTPROCESSING 
-        let composer = new EffectComposer(renderer);
-        let afterimagePass = new AfterimagePass();
 
-        (afterimagePass as any).uniforms['damp'].value = 1
-        composer.addPass(new RenderPass(scene, camera));
-        composer.addPass(afterimagePass);
+
+        // const glitchPass = new GlitchPass();
+        // composer.addPass(glitchPass);
 
 
 
@@ -240,6 +284,7 @@ const Hero = () => {
             camera.updateProjectionMatrix();
 
             renderer.setSize(sizes.width, sizes.height);
+            composer.setSize(sizes.width, sizes.height);
             renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
             composer.setSize(window.innerWidth, window.innerHeight);
         })
@@ -260,17 +305,13 @@ const Hero = () => {
 
 
         const animate = () => {
-            controls.update();
-            const elapsedTime = clock.getElapsedTime();
-            renderer.clear();
-
-            if (postEnabled) {
-                composer.render();
-            } else {
-                renderer.render(scene, camera);
-            }
-            renderer.render(scene, camera);
             requestAnimationFrame(animate);
+            controls.update();
+            composer.render();
+
+            // renderer.clear();
+            // const elapsedTime = clock.getElapsedTime();
+            // renderer.render(scene, camera);
         }
         animate()
         renderer.clear();
