6c6c1d000fde10a4fbc1e62674f6eae5006c1466
Klaatu98 <arabson1998@gmail.com>
2021-10-06 20:47:49 +0400

    add curtain js,fonts and colors

---
+++ components/pages/ColorDisplacement/ColorDisplacement.tsx
@@ -0,0 +1,154 @@
+//@ts-ignore
+import { Curtains, Plane, RenderTarget,ShaderPass } from 'curtainsjs';
+import { useEffect } from 'react';
+import { vertex } from "./shaders/vertex";
+import { fragment,rgbFs,blurFs } from "./shaders/fragment";
+
+
+
+
+const ColorDisplacement = ({ imagesArray }) => {
+
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const init = () => {
+        let scrollEffect = 0;
+
+        const curtains = new Curtains({
+            container: "canvas",
+            antialias: false,
+            pixelRatio: Math.min(1.5, window.devicePixelRatio)
+        })
+
+        curtains.onRender(() => {
+            scrollEffect = curtains.lerp(scrollEffect, 0, 0.05);
+        }).onScroll(() => {
+            const delta = curtains.getScrollDeltas();
+    
+            delta.y = -delta.y;
+    
+            // threshold
+            if (delta.y > 50) {
+                delta.y = 50;
+            }
+            else if (delta.y < -50) {
+                delta.y = -50;
+            }
+    
+            if (Math.abs(delta.y) > Math.abs(scrollEffect)) {
+                scrollEffect = curtains.lerp(scrollEffect, delta.y, 0.05);
+            }
+    
+        }).onError(() => {
+            document.body.classList.add("no-curtains");
+        }).onContextLost(() => {
+            curtains.restoreContext();
+        });
+    
+    
+        const planeElements = document.getElementsByClassName("plane");
+        const rgbTarget = new RenderTarget(curtains);
+
+
+
+
+        for(let i = 0; i < planeElements.length; i++) {
+            const plane = new Plane(curtains, planeElements[i], {
+                vertexShader: vertex(),
+                fragmentShader: fragment(),
+                texturesOptions: {
+                    minFilter: curtains.gl.LINEAR_MIPMAP_NEAREST
+                }
+            });
+    
+            plane.setRenderTarget(rgbTarget);
+        }
+
+
+        
+        const rgbPass = new ShaderPass(curtains, {
+            fragmentShader: rgbFs(),
+            renderTarget: rgbTarget,
+            uniforms: {
+                scrollEffect: {
+                    name: "uScrollEffect",
+                    type: "1f",
+                    value: 0,
+                },
+            },
+        });
+
+        rgbPass.onRender(() => {
+            rgbPass.uniforms.scrollEffect.value = scrollEffect;
+        });
+
+
+
+
+
+        let curtainsBBox = curtains.getBoundingRect();
+
+        const blurPass = new ShaderPass(curtains, {
+            fragmentShader: blurFs(),
+            uniforms: {
+                scrollEffect: {
+                    name: "uScrollEffect",
+                    type: "1f",
+                    value: 0,
+                },
+                resolution: {
+                    name: "uResolution",
+                    type: "2f",
+                    value: [curtainsBBox.width, curtainsBBox.height],
+                },
+            },
+        });
+    
+        blurPass.onRender(() => {
+            // update the uniform
+            blurPass.uniforms.scrollEffect.value = scrollEffect;
+        }).onAfterResize(() => {
+            curtainsBBox = curtains.getBoundingRect();
+            blurPass.uniforms.resolution.value = [curtainsBBox.width, curtainsBBox.height];
+        });
+    }
+
+    
+
+
+
+
+
+    return (
+        <>
+            <div id="page-wrap">
+                <div id="canvas"></div>
+
+                <div id="content">
+
+                    <div id="planes">
+                        {imagesArray.map(el => (
+                            <div key={el.id} className="plane-wrapper">
+                                <div className="plane-inner">
+                                    <div className="plane">
+                                        <img src={el.image} crossOrigin="" data-sampler="planeTexture" alt="pic" />
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+
+
+                    </div>
+
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ColorDisplacement;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
 // import Hero from "components/pages/landing/hero/Hero.landin";
 import Hero from "components/pages/landing/hero/Hero.landin";
 import ThreeDIcon from "components/pages/landing/icons/SingleIcon";
+import ColorDisplacement from "components/pages/ColorDisplacement/ColorDisplacement";
 
+const imagesArray = [
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1632338528496-877ff975d1a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80"
+    },
+    {
+        id: 2,
+        image: "https://images.unsplash.com/photo-1632613752851-9170e34f2479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 3,
+        image: "https://images.unsplash.com/photo-1632613752851-9170e34f2479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 4,
+        image: "https://images.unsplash.com/photo-1632613752851-9170e34f2479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    }
+    ,
+    {
+        id: 5,
+        image: "https://images.unsplash.com/photo-1632613752851-9170e34f2479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    }
+    ,
+    {
+        id: 6,
+        image: "https://images.unsplash.com/photo-1632613752851-9170e34f2479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    }
 
+]
 
 const index = () => {
   return (
     <>
       <Hero />
+      {/* <ColorDisplacement imagesArray={imagesArray}/> */}
       {/* <ThreeDIcon gltfModelPath='/3d_models/circle_with_orbit.glb' /> */}
     </>
   );
