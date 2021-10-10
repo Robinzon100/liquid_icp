dfc0fd3c4dd48341ca63590b9f677f503a7ee675
Klaatu98 <arabson1998@gmail.com>
2021-10-10 16:01:00 +0400

    add navigation and icon section

---
+++ components/pages/landing/2dIcons/TwoDIcons.tsx
@@ -0,0 +1,51 @@
+
+
+
+
+const TwoDIcons = () => {
+    const TwoDIconData = [
+        {
+            twoDIcons: "/images/full_circle.png",
+            heading: "Bridge from Dfinity to EVM compatible blockchains",
+        },
+        {
+            twoDIcons: "/images/inner_empty_circle.png",
+            heading: "Participate in rich DeFi ecosystems",
+        },
+        {
+            twoDIcons: "/images/cube.png",
+            heading: "Get insurance on your ICP holdings",
+        },
+        {
+            twoDIcons: "/images/triangle.png",
+            heading: "Lend, Borrow and earn 2x Rewards",
+        }
+
+    ]
+
+
+    return (
+        <>
+            <div className="twoDIcons-container">
+                <div className="twoDIcons-dark_container">
+                    {TwoDIconData.map((icon, i) => (
+                        <div className={`twoDIcon_container`} key={i}>
+
+                            <div className="icon"
+                                style={{ backgroundImage: `url(${icon.twoDIcons})` }}
+                            />
+
+                            <div className="twoDIcon_heading ">
+                                <h1>{icon.heading}</h1>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default TwoDIcons
