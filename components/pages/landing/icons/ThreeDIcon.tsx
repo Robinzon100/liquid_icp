4a68a093ba9c3177189f8fbeb8e705b184d0e10c
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-10-10 23:46:35 +0400

    :rocket: added post processing to the gltf objects

--- components/pages/landing/icons/ThreeDIcon.tsx
+++ components/pages/landing/icons/ThreeDIcon.tsx
@@ -1,10 +1,9 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { addGLFModel } from './utils/mesh.controller';
 import { createScene } from './utils/scene.contoller';
+import { Pane } from 'tweakpane';
 
 
-
-import ColorBalls from 'components/lib/ColorBalls/ColorBalls';
 
 const ThreeDIcon: FC = () => {
     const IconData = [
@@ -41,10 +40,10 @@ const ThreeDIcon: FC = () => {
     ]
 
 
-
     useEffect(() => {
         init()
     }, [])
+
 
 
     const init = () => {
@@ -57,125 +56,7 @@ const ThreeDIcon: FC = () => {
     return (
         <>
             <div className="icons_main">
-                <ColorBalls
-                    bgColor="var(--red)"
-                    className="main_ball"
-                    left="-7%"
-                    top="18%"
-                    width="16rem"
-                    height="18rem"
-                />
-                
-                 <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="-5%"
-                    top="5%"
-                    width="12rem"
-                    height="12rem"
-                />
-
-                <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="95%"
-                    top="18%"
-                    width="15rem"
-                    height="15rem"
-                />
-                <ColorBalls
-                    bgColor="var(--red)"
-                    className="main_ball"
-                    left="95%"
-                    top="5%"
-                    width="16rem"
-                    height="18rem"
-                />
-
-
-
-                <ColorBalls
-                    bgColor="var(--white)"
-                    className="main_ball"
-                    left="-7%"
-                    top="40%"
-                    width="16rem"
-                    height="16rem"
-                />
-                
-                 <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="-5%"
-                    top="55%"
-                    width="18rem"
-                    height="18rem"
-                />
-
-                <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="95%"
-                    top="40%"
-                    width="15rem"
-                    height="15rem"
-                />
-                <ColorBalls
-                    bgColor="var(--red)"
-                    className="main_ball"
-                    left="95%"
-                    top="55%"
-                    width="16rem"
-                    height="18rem"
-                />
-
-
-
-
-
-                <ColorBalls
-                    bgColor="var(--red)"
-                    className="main_ball"
-                    left="-7%"
-                    top="73%"
-                    width="16rem"
-                    height="18rem"
-                />
-                
-                 <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="-5%"
-                    top="89%"
-                    width="12rem"
-                    height="12rem"
-                />
-
-                <ColorBalls
-                    bgColor="var(--blue)"
-                    className="main_ball"
-                    left="95%"
-                    top="89%"
-                    width="15rem"
-                    height="15rem"
-                />
-                <ColorBalls
-                    bgColor="var(--white)"
-                    className="main_ball"
-                    left="95%"
-                    top="75%"
-                    width="16rem"
-                    height="18rem"
-                />
-
-
-
-
-
-
                 <div className="icons-container">
-
-
                     {IconData.map((icon, i) => (
                         <div className={`icon_container`} key={i}>
 
