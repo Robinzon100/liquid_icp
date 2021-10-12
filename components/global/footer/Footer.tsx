66ac60161caa68683540a995be14905786135545
Klaatu98 <arabson1998@gmail.com>
2021-10-12 13:52:37 +0400

    add partners section and footer

---
+++ components/global/footer/Footer.tsx
@@ -0,0 +1,64 @@
+const Footer = () => {
+
+    const socialsData = [
+        {
+            image: "/images/telegram.png",
+            url: "https://www.youtube.com/watch?v=lIQrUZLyATo&list=PLinuJh6RfGPMLw5fB4nXZoUYz9oebv9Bo&index=178&ab_channel=El2546"
+        },
+        {
+            image: "/images/discord.png",
+            url: "https://careerkarma.com/blog/bold-text-html/"
+        },
+        {
+            image: "/images/twitter.png",
+            url: "https://twitter.com/LiquidICP"
+        }
+
+    ]
+
+
+    return (
+        <>
+            <div className="footer">
+                <div className="footer_container">
+                    <div className="contact_us">
+                        <h1 className="f-size-h8 f-weight-l">Contact us <br />
+                        <a href="mailto:" target="#">
+                            <strong className="f-weight-bl">
+                                contact@icp-20.com
+                            </strong>
+                        </a>
+                        </h1>
+
+                    </div>
+                    <div className="socials">
+                        {socialsData.map((el, i) => (
+                            <a href={el.url} target="_blank" key={i}>
+                                <div className="social"
+                                    style={{ backgroundImage: `url(${el.image})` }}
+                                >
+
+                                </div>
+                            </a>
+
+                        ))}
+                    </div>
+
+
+
+                    <div className="documentation">
+                        <h1 className="f-size-h8 f-weight-l">Documentation <br />
+                            <a href="#" target="_blank">
+                                <strong className="f-weight-bl">
+                                    GitBook
+                                </strong>
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
--- components/pages/landing/Partners/Partners.tsx
+++ components/pages/landing/Partners/Partners.tsx
@@ -1,13 +1,58 @@
+
+
+
+
+
 const Partners = () => {
+
+    const partnersData = [
+        {
+            image: "/images/chain_link.png",
+            url:"#"
+        },
+        {
+            image: "/images/polygon.png",
+            url:"#"
+        },
+        {
+            image: "/images/dfinity.png",
+            url:"#"
+        },
+        {
+            image: "/images/aragon.png",
+            url:"#"
+        }
+    ]
+
+
+
     return (
         <>
             <div className="partners">
-                <div className="container">
-                    <div className="heading">
+
+                <div className="heading">
                     <h1 className="f-size-h1 f-weight-bo">
                         Partners
                     </h1>
-                    </div>
+                </div>
+
+
+                <div className="partners_container">
+                    {partnersData.map((el, i) => (
+                        <div className="partner"
+                            key={i}>
+
+                            <a href={el.url}>
+
+                                <div className="img"
+                                    style={{ backgroundImage: `url(${el.image})` }}
+                                />
+                            </a>
+
+                        </div>
+
+                    ))}
+
                 </div>
             </div>
         </>
