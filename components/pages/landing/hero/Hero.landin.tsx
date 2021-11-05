9bb1869a32a2ba0514dd8e0491d6186cd4adc573
Klaatu98 <arabson1998@gmail.com>
2021-11-05 20:20:14 +0400

    fix hero link

--- components/pages/landing/hero/Hero.landin.tsx
+++ components/pages/landing/hero/Hero.landin.tsx
@@ -552,7 +552,7 @@ const Hero = () => {
                         <div className="button_group animate_UI">
                             <Button
                                 style={{ cursor: "pointer" }}
-                                href="https://ido.icp-20.com/"
+                                href="https://ico.icp-20.com/"
                                 disabled={false}
                                 size={2.2}
                                 className="red_hero_btn f-size-p3 btn btn_gradient">
